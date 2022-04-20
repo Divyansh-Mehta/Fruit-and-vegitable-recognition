9667d208ab43ce428f12e7b5d5295bdfe1ee5c1c
Divyansh Mehta <72566559+Divyansh-Mehta@users.noreply.github.com>
2022-04-20 21:09:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,16 +41,18 @@ def processed_img(img_path):
 def run():
     st.title("Fruits🍍-Vegetable🍅 Classification")
     img_file = st.file_uploader("Choose an Image", type=["jpg", "png"])
-    if img_file is not None:
-        img = Image.open(img_file).resize((250,250))
-        st.image(img,use_column_width=False)
-        save_image_path = './upload_images/'+img_file.name
-       # with open(save_image_path, "wb") as f:
-        #    f.write(img_file.getbuffer())
+    img = Image.open(img_file).resize((250,250))
+
+    #if img_file is not None:
+     #   img = Image.open(img_file).resize((250,250))
+      #  st.image(img,use_column_width=False)
+       # save_image_path = './upload_images/'+img_file.name
+        #with open(save_image_path, "wb") as f:
+         #   f.write(img_file.getbuffer())
 
         # if st.button("Predict"):
         if img_file is not None:
-            result= processed_img(save_image_path)
+            result= processed_img(img)
             print(result)
             if result in vegetables:
                 st.info('**Category : Vegetables**')
